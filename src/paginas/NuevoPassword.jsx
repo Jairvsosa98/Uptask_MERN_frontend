cf83807f67dac72497ea72702efb5a52468de622
Jairvsosa98 <jairvsosa@gmail.com>
2023-01-07 17:41:24 -0500

    cambios en el proyecto front

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -60,7 +60,7 @@ const NuevoPassword = () => {
     const { msg } = alerta
     return (
         <>
-            <h1 className="text-sky-600 font-black text-6xl ">Reestablece tu password y no pierdas acceso a tus {''}<span className="text-slate-700">proyectos</span></h1>
+            <h1 className="text-sky-600 font-black text-5xl ">Reestablece tu password y no pierdas acceso a tus {''}<span className="text-slate-700">proyectos</span></h1>
             {msg && <Alerta alerta={alerta} />}
             {tokenValido && (
                 <form

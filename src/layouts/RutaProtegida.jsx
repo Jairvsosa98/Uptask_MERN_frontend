e05252c2f9f8f8a2cb4dce68e9e82198d4fc8186
Jair Augusto Villazón Sosa <56492766+Jairvsosa98@users.noreply.github.com>
2023-01-12 23:19:32 -0500

    Update RutaProtegida.jsx

--- src/layouts/RutaProtegida.jsx
+++ src/layouts/RutaProtegida.jsx
@@ -8,27 +8,25 @@ const RutaProtegida = () => {
 
   const { auth, cargando } = useAuth()
 
+   if (cargando) return "Cargando...";
 
   return (
     <>
-      {cargando ? 'Cargando...' : (
-
-          auth._id ? (
-            <div className="bg-gray-100">
-              <Header />
-              <div className="md:flex md:min-h-screen">
-                <Sidebar />
-                <main className="p-10 flex-1">
-                  <Outlet />
-                </main>
-              </div>
-
-            </div>
-          ) : <Navigate to="/" />
-        
+      {auth._id ? (
+        <div className="bg-gray-100">
+          <Header />
+          <div className="md:flex md:min-h-screen">
+            <Sidebar />
+            <main className="p-10 flex-1">
+              <Outlet />
+            </main>
+          </div>
+        </div>
+      ) : (
+        <Navigate to="/" />
       )}
     </>
-  )
-}
+  );
+};
 
 export default RutaProtegida
